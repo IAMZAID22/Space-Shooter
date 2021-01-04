18529eeceb7ff388558cd1f5d0d6e2dbe2accf69
IAMZAID22 <73696883+IAMZAID22@users.noreply.github.com>
2021-01-04 14:42:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -71,7 +71,7 @@ function draw(){
     as3.x = (Math.round(random(0)))
     as3.y = 0;
   }
-   if (keyDown("space") || mouseDown){
+   if (keyDown("space")){
     bl1.visible = true;
     bl1.x = pla.x;
     bl1.y = pla.y;
